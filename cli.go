d5153d9f8ecda1fd04ead02aafbe7104ab7f4ad2
loicsikidi <loic.sikidi@gmail.com>
2025-05-27 00:38:59 +0200

    🐣 init project

---
+++ cli.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+const (
+	BOOK_TOML = "book.toml"
+)
+
+type config struct {
+	Book struct {
+		Src string
+	}
+}
+
+var (
+	domain = flag.String("domain", "", "Domain of the target (e.g., https://example.com) [required]")
+	output = flag.String("output", "", "File to write the output to, defaults to stdout [optional]")
+)
+
+func main() {
+	flag.Parse()
+
+	if *domain == "" {
+		flag.Usage()
+		return
+	}
+
+	if strings.HasPrefix(*domain, "http") {
+		*domain = strings.Replace(*domain, "http", "https", 1)
+	} else if !strings.HasPrefix(*domain, "https") {
+		*domain = "https://" + *domain
+	}
+
+	targetDir := "src"
+	if _, err := os.Stat(getConfigPath()); err == nil {
+		cfg := new(config)
+		if _, err := toml.DecodeFile(getConfigPath(), cfg); err != nil {
+			fmt.Printf("Error reading %q: %v\n", BOOK_TOML, err)
+			os.Exit(1)
+		}
+		targetDir = cfg.Book.Src
+	}
+	paths, err := findPaths(targetDir, "")
+	if err != nil {
+		fmt.Printf("Error reading %q directory: %v\n", targetDir, errors.Unwrap(err))
+		os.Exit(1)
+	}
+
+	sitemap := NewSitemap(getUrls(*domain, paths))
+	out, err := sitemap.ToXML()
+	if err != nil {
+		fmt.Printf("Error generating sitemap: %v\n", err)
+		os.Exit(1)
+	}
+	switch {
+	case *output == "":
+		fmt.Println(string(out))
+	case *output != "":
+		if err := os.WriteFile(*output, out, 0644); err != nil {
+			fmt.Printf("Error writing to %q: %v\n", *output, err)
+			os.Exit(1)
+		}
+	}
+	os.Exit(0)
+}
+
+func findPaths(directory, currentPath string) ([]string, error) {
+	paths := []string{}
+
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			subPaths, err := findPaths(path.Join(directory, file.Name()), currentPath)
+			if err != nil {
+				return nil, err
+			}
+			paths = append(paths, subPaths...)
+		} else if strings.HasSuffix(file.Name(), ".md") && file.Name() != "SUMMARY.md" {
+			paths = append(paths, path.Join(currentPath, file.Name()))
+		}
+	}
+	return paths, nil
+}
+
+func getUrls(domain string, paths []string) []string {
+	urls := []string{domain}
+	for _, p := range paths {
+		filename := strings.Replace(p, ".md", ".html", 1)
+		urls = append(urls, fmt.Sprintf("%s/%s", domain, filename))
+	}
+	return urls
+}
+
+func getConfigPath() string {
+	wd, _ := os.Getwd()
+	return path.Join(wd, BOOK_TOML)
+}
---
+++ sitemap.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+type Url struct {
+	Localization string `xml:"loc"`
+	Priority     string `xml:"priority"`
+}
+
+type Sitemap struct {
+	XMLName string `xml:"urlset"`
+	Urls    []Url  `xml:"urls"`
+	Xmlns   string `xml:"xmlns,attr"`
+}
+
+func NewSitemap(urls []string) *Sitemap {
+	u := make([]Url, len(urls))
+	for i, url := range urls {
+		u[i] = Url{
+			Localization: url,
+			Priority:     fmt.Sprintf("%.1f", 1.0),
+		}
+	}
+	return &Sitemap{
+		Xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+		Urls:  u,
+	}
+}
+
+func (s *Sitemap) ToXML() ([]byte, error) {
+	output, err := xml.MarshalIndent(s, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}

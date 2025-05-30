5b3ae4f164e8e356d90ba4c457aa42bca814a307
loicsikidi <loic.sikidi@gmail.com>
2025-05-30 18:27:58 +0200

    feat: ✨ support mdbook plugin interface

--- .nix/binary.nix
+++ .nix/binary.nix
@@ -2,5 +2,5 @@
 pkgs.buildGo124Module rec {
   name = "mdbook-sitemap-generator";
   src = pkgs.lib.cleanSource ../.;
-  vendorHash = "sha256-h5UXs7ujP0YIBKusstTDXvWBhqKpYs6HtzzdaB1+6Wg=";
+  vendorHash = "sha256-WUQW8EDJ7kT2CUZsNtlVUVwwqFRHkpkU6pFmx7/MDGg=";
 }
--- cli.go
+++ cli.go
@@ -11,80 +11,178 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/imjasonh/version"
+	mdbook "github.com/ngyewch/mdbook-plugin"
 )
+
+type mode string
 
 const (
-	BOOK_TOML = "book.toml"
+	standalone mode = "standalone" // running as a standalone CLI tool
+	plugin     mode = "plugin"     // running as a plugin for mdbook
+
+	BOOK_TOML    = "book.toml"
+	pluginName   = "sitemap-generator"
+	domainOption = "domain"
 )
 
-type config struct {
+type options struct {
+	Domain    string
+	Output    string
+	Version   bool
+	mode      mode
+	pluginCtx *mdbook.RenderContext
+}
+
+func (o *options) SanitizeDomain() error {
+	u, err := url.Parse(o.Domain)
+	if err != nil {
+		return fmt.Errorf("error: invalid value %q: %w", o.Domain, err)
+	}
+	if u.Scheme == "http" {
+		o.Domain = strings.Replace(o.Domain, "http", "https", 1)
+	} else if u.Scheme != "https" {
+		o.Domain = "https://" + o.Domain
+	}
+	return nil
+}
+
+// representation of the book configuration (i.e., book.toml)
+type bookConfig struct {
 	Book struct {
 		Src string
 	}
 }
 
-var (
-	domain   = flag.String("domain", "", "Domain of the target (e.g., https://example.com) [required]")
-	output   = flag.String("output", "", "File to write the output to, defaults to stdout [optional]")
-	_version = flag.Bool("version", false, "Print the version of the CLI [optional]")
-)
-
 func main() {
-	flag.Parse()
+	opts, err := getOptions()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	if *_version {
+	if opts.mode == standalone && opts.Version {
 		fmt.Println(version.Get())
 		os.Exit(0)
 	}
 
-	if *domain == "" {
-		flag.Usage()
-		return
-	}
-
-	u, err := url.Parse(*domain)
-	if err != nil {
-		fmt.Printf("Error parsing domain %q: %v\n", *domain, err)
-		os.Exit(1)
-	}
-
-	if u.Scheme == "http" {
-		*domain = strings.Replace(*domain, "http", "https", 1)
-	} else if u.Scheme != "https" {
-		*domain = "https://" + *domain
-	}
-
-	targetDir := "src"
-	if _, err := os.Stat(getConfigPath()); err == nil {
-		cfg := new(config)
-		if _, err := toml.DecodeFile(getConfigPath(), cfg); err != nil {
-			fmt.Printf("Error reading %q: %v\n", BOOK_TOML, err)
-			os.Exit(1)
+	if opts.Domain == "" {
+		if opts.mode == standalone {
+			flag.Usage()
+		} else {
+			fmt.Println("Domain is required. Please provide the domain in book.toml")
 		}
-		targetDir = cfg.Book.Src
-	}
-	paths, err := findPaths(targetDir, "")
-	if err != nil {
-		fmt.Printf("Error reading %q directory: %v\n", targetDir, errors.Unwrap(err))
 		os.Exit(1)
 	}
 
-	sitemap := NewSitemap(getUrls(*domain, paths))
+	if err := opts.SanitizeDomain(); err != nil {
+		fmt.Printf("Error sanitizing domain: %v\n", err)
+		os.Exit(1)
+	}
+
+	paths, err := getPaths(opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	sitemap := NewSitemap(getUrls(opts.Domain, paths))
 	out, err := sitemap.ToXML()
 	if err != nil {
 		fmt.Printf("Error generating sitemap: %v\n", err)
 		os.Exit(1)
 	}
 	switch {
-	case *output == "":
+	case opts.Output == "":
 		fmt.Println(string(out))
-	case *output != "":
-		if err := os.WriteFile(*output, out, 0644); err != nil {
-			fmt.Printf("Error writing sitemap content to %q: %v\n", *output, err)
+	case opts.Output != "":
+		if err := os.WriteFile(opts.Output, out, 0644); err != nil {
+			fmt.Printf("Error writing sitemap content to %q: %v\n", opts.Output, err)
 			os.Exit(1)
 		}
 	}
 	os.Exit(0)
+}
+
+func getOptions() (*options, error) {
+	var (
+		opts    *options
+		domain  = flag.String(domainOption, "", "Domain of the target (e.g., 'example.com') [required]")
+		output  = flag.String("output", "", "File to write the output to, defaults to stdout [optional]")
+		version = flag.Bool("version", false, "Print the version of the CLI [optional]")
+	)
+	flag.Parse()
+
+	if *domain == "" && *output == "" && !*version {
+		// fallback to plugin mode if no flags are provided
+		ctx, err := mdbook.ParseRenderContextFromReader(os.Stdin)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing stdin: %w", err)
+		}
+		domain, err := getDomainFromPluginContext(ctx)
+		if err != nil {
+			return nil, err
+		}
+		opts = &options{
+			mode:      plugin,
+			Domain:    domain,
+			Output:    path.Join(ctx.Destination, "sitemap.xml"),
+			pluginCtx: ctx,
+		}
+	} else {
+		opts = &options{
+			Domain:  *domain,
+			Output:  *output,
+			Version: *version,
+			mode:    standalone,
+		}
+	}
+	return opts, nil
+}
+
+func getDomainFromPluginContext(ctx *mdbook.RenderContext) (string, error) {
+	if ctx == nil || ctx.Config == nil || ctx.Config.Output == nil {
+		return "", fmt.Errorf("missing 'Config.Output' in plugin context")
+	}
+	for k, v := range ctx.Config.Output {
+		if k != pluginName {
+			continue
+		}
+		pluginCfg := v.(map[string]any)
+		for kk, vv := range pluginCfg {
+			if kk == domainOption {
+				if domain, ok := vv.(string); ok {
+					return domain, nil
+				}
+				return "", fmt.Errorf("'domain' value must be a string")
+			}
+		}
+	}
+	return "", fmt.Errorf("'domain' is not defined in the `[output.%s]` section of the book.toml", pluginName)
+}
+
+func getPaths(options *options) ([]string, error) {
+	if options.mode == standalone {
+		return getPathsFromStandalone()
+	} else if options.mode == plugin {
+		return getPathsFromPluginContext(options.pluginCtx)
+	}
+	return nil, fmt.Errorf("unsupported mode: %q", options.mode)
+}
+
+func getPathsFromStandalone() ([]string, error) {
+	targetDir := "src"
+	if _, err := os.Stat(getbookConfigPath()); err == nil {
+		cfg := new(bookConfig)
+		if _, err := toml.DecodeFile(getbookConfigPath(), cfg); err != nil {
+			return nil, fmt.Errorf("error reading %q: %v\n", BOOK_TOML, err)
+		}
+		targetDir = cfg.Book.Src
+	}
+	paths, err := findPaths(targetDir, "")
+	if err != nil {
+		return nil, fmt.Errorf("error reading %q directory: %v\n", targetDir, errors.Unwrap(err))
+	}
+	return paths, nil
 }
 
 func findPaths(directory, currentPath string) ([]string, error) {
@@ -108,6 +206,26 @@ func findPaths(directory, currentPath string) ([]string, error) {
 	return paths, nil
 }
 
+func getPathsFromPluginContext(ctx *mdbook.RenderContext) ([]string, error) {
+	if ctx == nil || ctx.Book == nil || ctx.Book.Sections == nil {
+		return nil, fmt.Errorf("invalid plugin context: missing Book or Sections")
+	}
+	paths := findPathsFromPluginContext(ctx.Book.Sections)
+	return paths, nil
+}
+
+func findPathsFromPluginContext(items []*mdbook.BookItem) []string {
+	paths := []string{}
+	for _, item := range items {
+		paths = append(paths, item.Chapter.SourcePath)
+		if len(item.Chapter.SubItems) > 0 {
+			subPaths := findPathsFromPluginContext(item.Chapter.SubItems)
+			paths = append(paths, subPaths...)
+		}
+	}
+	return paths
+}
+
 func getUrls(domain string, paths []string) []string {
 	urls := []string{domain}
 	for _, p := range paths {
@@ -117,7 +235,7 @@ func getUrls(domain string, paths []string) []string {
 	return urls
 }
 
-func getConfigPath() string {
+func getbookConfigPath() string {
 	wd, _ := os.Getwd()
 	return path.Join(wd, BOOK_TOML)
 }

911742b234c3dadb913710b6842972ae600c8f7c
loicsikidi <loic.sikidi@gmail.com>
2025-11-03 18:09:42 +0100

    refactor: harmonize git-hooks.nix usage

--- default.nix
+++ default.nix
@@ -1,4 +1,3 @@
-{ pkgs ? import <nixpkgs> { } }: {
-  binary = pkgs.callPackage ./.nix/binary.nix { };
-  pre-commit-check = pkgs.callPackage ./.nix/pre-commit.nix { };
+{pkgs ? import <nixpkgs> {}}: {
+  binary = pkgs.callPackage ./.nix/binary.nix {};
 }

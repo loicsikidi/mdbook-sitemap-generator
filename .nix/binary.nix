e2a7f8b6862e8110d2d4b3f7d42dee8a9a0a49ee
loicsikidi <loic.sikidi@gmail.com>
2026-05-08 13:25:31 +0200

    fix

--- .nix/binary.nix
+++ .nix/binary.nix
@@ -1,5 +1,5 @@
 {pkgs}:
-pkgs.buildGoModule rec {
+pkgs.buildGoModule {
   name = "mdbook-sitemap-generator";
   src = pkgs.lib.cleanSource ../.;
   vendorHash = "sha256-5uDi/9YlUNRDQKFROlN8sLvLRFFOvZvZHcmR6fARG5Q=";
